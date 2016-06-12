21bf4460e5f285e604a7a099d2ae12b6f90146c4
Garron Sanchez <garron.michael@gmail.com>
2016-06-11 23:56:13 -0700

    add g signature

--- client/src/script.js
+++ client/src/script.js
@@ -1,83 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Content extends Component {
-
-  constructor(props) {
-    super(props);
-    this.profilePhotoTransition = this.profilePhotoTransition.bind(this);
-  }
-
-  profilePhotoTransition() {
-    const photo = this.refs.profilePhoto;
-    photo.style.width = 0;
-    setTimeout(() => {
-      photo.style.width = '500px';
-    }, 1000);
-  }
-
-  render() {
-    return (
-      <div
-        style={{
-          alignItems: 'center',
-          display: 'flex',
-          justifyContent: 'space-around',
-        }}
-      >
-        <div>
-          <img
-            alt="Garron at Poinsettia Park in West Hollywood, CA."
-            onClick={this.profilePhotoTransition}
-            ref="profilePhoto"
-            style={{
-              borderRadius: '50%',
-              height: 500,
-              transition: 'all 1s',
-              width: 500,
-            }}
-            src="./../assets/images/bball.jpg"
-          />
-          <div>
-            <i className="fa fa-twitter" />
-            <i className="fa fa-github" />
-            <i className="fa fa-email" />
-          </div>
-        </div>
-        <div>
-          <div>Hello, I'm Garron Sanchez. I am a software engineer in West Hollywood, CA.</div>
-          <div
-            style={{
-              alignItems: 'center',
-              display: 'flex',
-              justifyContent: 'center',
-            }}
-          >
-          Made with
-            <img
-              alt="React logo."
-              style={{
-                height: 20,
-                marginLeft: 5,
-                width: 20,
-              }}
-              src="./../assets/images/react.png"
-            />
-          &
-            <img
-              alt="React logo."
-              style={{
-                height: 20,
-                marginLeft: 5,
-                width: 20,
-              }}
-              src="./../assets/images/node.png"
-            />
-          </div>
-        </div>
+function Content() {
+  return (
+    <div
+      style={{
+        alignItems: 'center',
+        backgroundImage: 'url(./../assets/images/palm_tree.jpg)',
+        backgroundOrigin: 'border-box',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '100%',
+        color: '#fff',
+        display: 'flex',
+        fontFamily: 'Helvetica Nueue, sans-serif',
+        fontWeight: 100,
+        height: '100vh',
+        position: 'relative',
+        textAlign: 'center',
+      }}
+    >
+      <div>
+        <img
+          alt="Garron's signature"
+          height="600px"
+          src="./../assets/images/g.png"
+        />
       </div>
-    );
-  }
+      <div>
+        <span
+          style={{
+            fontWeight: 600,
+            marginRight: 5,
+          }}
+        >
+          Hello, I'm Garron.
+        </span>
+        Software engineer in West Hollywood, CA.
+      </div>
+    </div>
+  );
 }
 
 
