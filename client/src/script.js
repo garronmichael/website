2df69c37b836ca29b4e03ae569ad28f092e4b3bc
Garron Sanchez <garron.michael@gmail.com>
2016-06-22 22:38:23 -0700

    add Radium

--- client/src/script.js
+++ client/src/script.js
@@ -1,94 +1,108 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Radium, { StyleRoot } from 'radium';
 
-function Content() {
-  return (
-    <div
-      style={{
-        backgroundImage: 'url(./../assets/images/palm_tree.jpg)',
-        backgroundOrigin: 'border-box',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '100%',
-        height: '100vh',
-      }}
-    >
+class Content extends React.Component {
+  render() {
+    return (
       <div
         style={{
-          alignItems: 'center',
-          color: '#fff',
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '50%',
+          backgroundImage: 'url(./../assets/images/palm_tree.jpg)',
+          backgroundOrigin: 'border-box',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
         }}
       >
         <div
           style={{
-            marginBottom: 30,
-          }}
-        >
-          <div
-            style={{
-              fontFamily: 'Lobster, sans-serif',
-              fontSize: 80,
-              marginBottom: 20,
-            }}
-          >
-            Hello, I'm Garron.
-          </div>
-          <div
-            style={{
-              fontFamily: 'Helvetica Nueue, sans-serif',
-              fontSize: 16,
-              fontWeight: 300,
-            }}
-          >
-            Software engineer and lifelong learner in West Hollywood, CA.
-          </div>
-        </div>
-        <div
-          style={{
             alignItems: 'center',
+            color: '#fff',
             display: 'flex',
-            justifyContent: 'space-around',
-            width: '80%',
+            flexDirection: 'column',
+            height: '100%',
+            justifyContent: 'center',
+            textAlign: 'center',
+            width: '100%',
           }}
         >
-          <img
-            alt="react logo"
-            height="50px"
-            width="50px"
-            src="./../assets/images/react.png"
-          />
-          <img
-            alt="node logo"
-            height="50px"
-            width="50px"
-            src="./../assets/images/node.png"
-          />
-          <img
-            alt="j s dot l a logo"
-            height="50px"
-            width="50px"
-            src="./../assets/images/jsla.png"
-          />
-          <img
-            alt="rainbow"
-            height="50px"
-            width="50px"
-            src="./../assets/images/rainbow.png"
-          />
+          <div
+            style={{
+              marginBottom: 30,
+            }}
+          >
+            <div
+              style={{
+                fontFamily: 'Lobster, sans-serif',
+                fontSize: 80,
+                marginBottom: 20,
+              }}
+            >
+              Hello, I'm Garron.
+            </div>
+            <div
+              style={{
+                fontFamily: 'Helvetica Nueue, sans-serif',
+                fontSize: 16,
+                fontWeight: 300,
+              }}
+            >
+              Software engineer and lifelong learner in West Hollywood, CA.
+            </div>
+          </div>
+          <div
+            style={{
+              alignItems: 'center',
+              display: 'flex',
+              justifyContent: 'space-around',
+              width: '80%',
+            }}
+          >
+            <img
+              alt="react logo"
+              height="50px"
+              width="50px"
+              src="./../assets/images/react.png"
+            />
+            <img
+              alt="node logo"
+              height="50px"
+              width="50px"
+              src="./../assets/images/node.png"
+            />
+            <img
+              alt="j s dot l a logo"
+              height="50px"
+              width="50px"
+              src="./../assets/images/jsla.png"
+            />
+            <img
+              alt="rainbow"
+              height="50px"
+              width="50px"
+              src="./../assets/images/rainbow.png"
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+Content = Radium(Content);
+
+class App extends React.Component {
+  render() {
+    return (
+      <StyleRoot>
+        <Content />
+      </StyleRoot>
+    );
+  }
 }
 
 
 ReactDOM.render(
-  <Content />,
+  <App />,
   document.getElementById('content')
 );
