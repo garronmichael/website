5c565cc7eecb34ccfd625bce45de950fae3acc9f
Garron Sanchez <garron.michael@gmail.com>
2017-10-16 19:55:22 -0700

    express must send back index.html for react router to work

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 const port = process.env.PORT || 8080
 
 app.use(express.static('dist'))
+
+app.get('*', (req, res) => {
+
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
+
 app.listen(port)
