0bc3b0bd49326049cf08f190fefb28b155063191
Garron Sanchez <garron.michael@gmail.com>
2017-10-16 19:54:56 -0700

    add react router

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,20 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom'
 
 import Home from './components/home.jsx'
 
-ReactDOM.render(
-  <Home />,
+ReactDOM.render((
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route component={null} />
+    </Switch>
+  </BrowserRouter>
+),
   document.getElementById('content')
 )
