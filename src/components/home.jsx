4566c7f647bae292e38c17e056425913aec01c94
Garron Sanchez <garron.michael@gmail.com>
2017-10-17 00:42:32 -0700

    pull background outside of dynamic routes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Radium from 'radium'
 import {withRouter} from 'react-router-dom'
 
-import PalmTreeImage from '../assets/images/palm_tree.jpg'
 import RainbowImage from '../assets/images/rainbow.png'
 import NodeImage from '../assets/images/node.png'
 import ReactImage from '../assets/images/react.png'
@@ -16,104 +15,94 @@ class Home extends React.Component {
     return (
       <div
         style={{
-          backgroundImage: `url(${PalmTreeImage})`,
-          backgroundOrigin: 'border-box',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
+          alignItems: 'center',
+          color: '#b4c5da',
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          textAlign: 'center',
+          justifyContent: 'space-around',
+          width: '100%'
         }}
       >
-        <div
-          style={{
-            alignItems: 'center',
-            color: '#b4c5da',
-            display: 'flex',
-            flexDirection: 'column',
-            height: '100%',
-            textAlign: 'center',
-            justifyContent: 'space-around',
-            width: '100%'
-          }}
-        >
-          <div>
-            <div
-              style={{
-                fontFamily: 'Lobster, sans-serif',
-                fontSize: '3em',
-                marginBottom: 30,
-                '@media (min-width: 769px)': {
-                  fontSize: '5em'
-                }
-              }}
-            >
-              Hello, I'm Garron.
-            </div>
-            <div
-              style={{
-                fontFamily: 'Lobster, sans-serif',
-                fontSize: '2em',
-                '@media (min-width: 769px)': {
-                  fontSize: '3em'
-                }
-              }}
-            >
-              Software Engineer
-            </div>
-          </div>
+        <div>
           <div
             style={{
-              alignItems: 'center',
-              display: 'flex',
-              justifyContent: 'space-around',
-              width: '80%'
-            }}
-          >
-            <img
-              alt="react logo"
-              height="50px"
-              width="50px"
-              src={ReactImage}
-            />
-            <img
-              alt="node logo"
-              height="50px"
-              width="50px"
-              src={NodeImage}
-            />
-            <img
-              alt="j s dot l a logo"
-              height="50px"
-              width="50px"
-              src={JSLAImage}
-            />
-            <img
-              alt="rainbow"
-              height="50px"
-              width="50px"
-              src={RainbowImage}
-            />
-          </div>
-          <button
-            onClick={() => this.props.history.push('/hire')}
-            style={{
-              border: '3px solid #cbb2bb',
-              borderRadius: 10,
-              backgroundColor: 'transparent',
-              color: '#cbb2bb',
               fontFamily: 'Lobster, sans-serif',
-              width: '65%',
-              transition: 'all 0.5s',
-              cursor: 'pointer',
               fontSize: '3em',
-              ':hover': {
-                backgroundColor: '#cbb2bb',
-                color: '#fff'
+              marginBottom: 30,
+              '@media (min-width: 769px)': {
+                fontSize: '5em'
               }
             }}
           >
-            Hire
-          </button>
+            Hello, I'm Garron.
+          </div>
+          <div
+            style={{
+              fontFamily: 'Lobster, sans-serif',
+              fontSize: '2em',
+              '@media (min-width: 769px)': {
+                fontSize: '3em'
+              }
+            }}
+          >
+            Software Engineer
+          </div>
         </div>
+        <div
+          style={{
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'space-around',
+            width: '80%'
+          }}
+        >
+          <img
+            alt="react logo"
+            height="50px"
+            width="50px"
+            src={ReactImage}
+          />
+          <img
+            alt="node logo"
+            height="50px"
+            width="50px"
+            src={NodeImage}
+          />
+          <img
+            alt="j s dot l a logo"
+            height="50px"
+            width="50px"
+            src={JSLAImage}
+          />
+          <img
+            alt="rainbow"
+            height="50px"
+            width="50px"
+            src={RainbowImage}
+          />
+        </div>
+        <button
+          onClick={() => this.props.history.push('/hire')}
+          style={{
+            border: '3px solid #cbb2bb',
+            borderRadius: 10,
+            backgroundColor: 'transparent',
+            color: '#cbb2bb',
+            fontFamily: 'Lobster, sans-serif',
+            width: '65%',
+            transition: 'all 0.5s',
+            cursor: 'pointer',
+            fontSize: '3em',
+            ':hover': {
+              backgroundColor: '#cbb2bb',
+              color: '#fff'
+            }
+          }}
+        >
+          Hire
+        </button>
       </div>
     )
   }
--- src/index.jsx
+++ src/index.jsx
@@ -10,14 +10,26 @@ import {StyleRoot} from 'radium'
 import Home from './components/home.jsx'
 import Hire from './components/hire.jsx'
 
+import PalmTreeImage from './assets/images/palm_tree.jpg'
+
 ReactDOM.render((
   <StyleRoot>
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/hire" component={Hire} />
-        <Route component={null} />
-      </Switch>
+      <div
+        style={{
+          backgroundImage: `url(${PalmTreeImage})`,
+          backgroundOrigin: 'border-box',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+      >
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/hire" component={Hire} />
+          <Route component={null} />
+        </Switch>
+      </div>
     </BrowserRouter>
   </StyleRoot>
 ),
